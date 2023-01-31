7d1bdc4a12a34ec62c91fae7cfe0ca0ac2d4c921
Guudz <Kilian1702@outlook.com>
2023-01-31 05:01:29 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+this_is_a_map 'yes'
+lua54 'yes'
+
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+escrow_ignore {
+  'stream/streetsign.ydr',
+  'stream/imperialsign.ydr',
+  'stream/swig.ydr',
+  'stream/tas.ydr',
+  'stream/ground.ydr',
+  'stream/compproj01_la.ydr',
+}
